b515abab1e7089e3b77d3d8c0bfb3c54d7539ecb
Nelgar20 <black_w@hotmail.it>
2026-08-18 23:30:59 +0100

    Start create templates boilerplate

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Article
 
-def index(request):
-    return HttpResponse("Welcome to the blog")
+
+class ArticleList(generic.ListView):
+    queryset = Article.objects.all()
+    template = "article_list.html"
