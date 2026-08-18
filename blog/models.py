1861fc36954cca615efce9a69deda2ccdd45c15f
Nelgar20 <black_w@hotmail.it>
2026-08-18 14:53:56 +0100

    Add and migrate Comment model to database

--- blog/models.py
+++ blog/models.py
@@ -31,4 +31,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
     
+    def __str__(self):
+        return f"Commented by {self.author}"
