64145e6f37e12d70969be3f8cefd8d6012e2fbb8
Nelgar20 <black_w@hotmail.it>
2026-08-18 13:17:09 +0100

    create Post and Category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    exerpt = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="posts"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+        )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
+    
