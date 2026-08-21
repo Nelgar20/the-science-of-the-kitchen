60f1fef764fbe498940786b69b9fb3ba4192d9a8
Nelgar20 <black_w@hotmail.it>
2026-08-21 12:23:29 +0100

    Create article_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='blog'),
+    path('<slug:slug>/', views.article_detail, name='article_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 
@@ -7,3 +7,27 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.all()
     template = "article_list.html"
     paginate_by = 6
+
+
+def article_detail(request, slug):
+    """
+    Display an individual :model:`blog.Article`.
+
+    **Context**
+
+    ``article``
+        An instance of :model:`blog.Article`.
+
+    **Template:**
+
+    :template:`blog/article_detail.html`
+    """
+
+    queryset = Article.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/article_detail.html",
+        {"article": article},
+    )
