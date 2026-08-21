404dc2d0d96cac551e51baff96506bb82cdbafaf
Nelgar20 <black_w@hotmail.it>
2026-08-21 16:17:13 +0100

    create about.htm boilerplate

--- about/admin.py
+++ about/admin.py
@@ -3,4 +3,7 @@ from .models import About
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
 
+    summernote_fields = ('content',)
